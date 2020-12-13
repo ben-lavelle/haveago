c34cfbaf3df48ee8fb55f4431a680575f600bd62
ben-lavelle <benjamin.lavelle@justice.gov.uk>
2020-12-13 14:02:02 +0000

    Seed random greetings to avoid auto Seed(1)

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 // Go exports names that start with capitals for use
@@ -19,6 +20,12 @@ func Hello(name string) (string, error) {
 	// var message string; message = ...
 	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// If a pre-run init with Seed isn't called, rand
+// assumes Seed(1) -- always the same output.
+func init() {
+	rand.Seed(time.Now().UnixNano())
 }
 
 func randomFormat() string {
