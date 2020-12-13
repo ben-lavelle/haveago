f900c5fbd02860a54b19fd113f74fab8650ee809
ben-lavelle <benjamin.lavelle@justice.gov.uk>
2020-12-13 13:52:47 +0000

    Add error handling to greetings

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,15 +1,22 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Pants
 // Go exports names that start with capitals for use
 // outside the package.
 
 // Hello returns a greeting for a named person.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// If no name is given, return an error
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 	// := declares and initialises; could've used:
 	// var message string; message = ...
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
--- hello/hello.go
+++ hello/hello.go
@@ -4,11 +4,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"bjml.uk/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Benjamin")
+	// Set up a Logger
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting
+	message, err := greetings.Hello("Benjamin")
+	// If an error is returned, print it and quit.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If not, print the message
 	fmt.Println(message)
 }
